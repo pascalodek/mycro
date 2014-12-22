d1fe9c39aff765b878e9109b5b933b08d60b07db
Pascal Odek <pascalo@stanford.edu>
2014-12-21 23:57:26 -0600

    initial swift files

---
+++ Mycro/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Mycro
+//
+//  Created by PASCAL ARINGO ODEK on 12/21/14.
+//  Copyright (c) 2014 Pascal Odek. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
